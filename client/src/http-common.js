ae0b18866bd0062b1bc6909473a2b35b15237a0d
swathi1316 <swathi.velicheti@gmail.com>
2023-05-12 00:51:03 -0400

    client side url problem

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://movie-verse.herokuapp.com",
+    baseURL: "https://movie-verse.herokuapp.com/",
   headers: {
     "Content-type": "application/json"
   }
