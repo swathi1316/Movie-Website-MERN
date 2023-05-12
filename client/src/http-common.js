9f7e8df5cf28fbc74f9ac62a257b6392cb50eb25
swathi1316 <swathi.velicheti@gmail.com>
2023-05-12 08:44:03 -0400

    changing server.js

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "",
+    baseURL: "https://movie-verse.herokuapp.com",
   headers: {
     "Content-type": "application/json"
   }
