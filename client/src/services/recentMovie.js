6b35109140b31251e9743635f31bedf45e8f3093
swathi1316 <swathi.velicheti@gmail.com>
2023-05-12 09:23:23 -0400

    changing server.js

--- client/src/services/recentMovie.js
+++ client/src/services/recentMovie.js
@@ -2,6 +2,7 @@ import http from '../http-common.js';
 
 export const latestMovies = async () => {
   const response = await http.get("/");
+  console.log("response",response,response.data);
   return response.data;
 };
 
--- server.js
+++ server.js
@@ -11,16 +11,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const prod = process.env.NODE_ENV
+console.log("prod",prod);
 if (process.env.NODE_ENV === "production") {
   console.log("going in");
-  app.use("/", express.static("./client/build"));
+  app.use("/", express.static("client/build"));
 }
 else
 {
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
 }
+
+
 
 dbConnect()
   .then(() => {
